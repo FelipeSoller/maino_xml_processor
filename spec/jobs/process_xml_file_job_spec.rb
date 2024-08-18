b60348c460e0b0e92ddbf77fe6cd3303142e152e
Felipe Soller <soller@outlook.com>
2024-08-18 00:41:54 -0300

    fix: process xml file job

--- spec/jobs/process_xml_file_job_spec.rb
+++ spec/jobs/process_xml_file_job_spec.rb
@@ -4,20 +4,22 @@ require 'zip'
 RSpec.describe ProcessXmlFileJob, type: :job do
   let(:user) { create(:user) }
   let(:document) { create(:document, user:) }
+  let(:xml_file_path) { Rails.root.join('spec/fixtures/sample.xml') }
+  let(:blob) { ActiveStorage::Blob.create_and_upload!(io: File.open(xml_file_path), filename: 'sample.xml', content_type: 'application/xml') }
 
   describe '#perform' do
     context 'when the file is an xml' do
-      before do
-        allow(document.file).to receive(:file).and_return(double(extension: 'xml'))
-        allow(File).to receive(:read).and_return(File.read(Rails.root.join('spec/fixtures/sample.xml')))
-      end
-
       it 'processes the xml file and creates document details' do
-        expect { described_class.perform_now(document.id) }
-          .to change { DocumentDetail.count }.by(1)
-          .and change { Emit.count }.by(1)
-          .and change { Dest.count }.by(1)
-          .and change { Det.count }.by(2)
+        # Assinando o arquivo
+        file_signed_id = blob.signed_id
+
+        # Chamando o job
+        expect do
+          described_class.perform_now(file_signed_id, document.id)
+        end.to change { DocumentDetail.count }.by(1)
+                                              .and change { Emit.count }.by(1)
+                                                                        .and change { Dest.count }.by(1)
+                                                                                                  .and change { Det.count }.by(2)
       end
     end
   end
